265740a50219e8ab927cb4f40919af20e5c8ccc1
Higgins00 <higginsmichael93@gmail.com>
2021-03-19 13:58:56 -0400

    probabilities

--- Disentangler_Draft/Near_FinalDraft.py
+++ Disentangler_Draft/Near_FinalDraft.py
@@ -331,7 +331,8 @@ class PixelMapFit:
                                  source = np.asarray(gaia_data['source']),
                                  x = np.asarray(gaia_data['x']),
                                  y = np.asarray(gaia_data['y']),
-                                 distance = np.square(self.x-gaia_data['x'])+np.square(self.y-gaia_data['y']))
+                                 distance = np.square(self.x-gaia_data['x'])+np.square(self.y-gaia_data['y']),
+                                 probability = stats.norm.sf(distance,scale=np.sqrt(self.result.params['x'].stderr**2 +self.result.params['y'].stderr**2 ))
                     starlist = pd.DataFrame.from_dict(stars)
                     self.stars = starlist.sort_values(by=[r'distance'])
                     
