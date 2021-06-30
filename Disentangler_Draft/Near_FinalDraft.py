020399826d1ea6abc22236d02dd3febc98bb78c6
Higgins00 <higginsmichael93@gmail.com>
2021-06-30 19:17:36 -0400

    sextuple code

--- Disentangler_Draft/Near_FinalDraft.py
+++ Disentangler_Draft/Near_FinalDraft.py
@@ -1,67 +1,107 @@
-
 # coding: utf-8
-
-# In[ ]:
-
 
 from __future__ import division, print_function
 import logging
 import warnings
 import numpy as np
-from astropy.stats import sigma_clip
 import os
 from astropy.coordinates import SkyCoord, Angle
 import astropy.units as u
 import matplotlib.pyplot as plt
 import pandas as pd
-import scipy as sp
 import lightkurve as lk
-from scipy import stats
 from astropy import units as u
-import scipy.optimize as opt
-from photutils import centroids as cent
 import lmfit as lm
 from lmfit import Minimizer, Parameters, report_fit
+import PRF
 
 
 class PixelMapFit:
-    """Object to hold 2D array of lk.periodgram object data .
-    Attributes
+    """Class designed to give users access to analysis functions.
+    
+    Parameters
     ----------
     targetpixelfile : targetpixelfile object
-        
-    Optional keywords accepted if ``method='lombscargle'`` are:
-            ``minimum_frequency``, ``maximum_frequency``, ``minimum_period``,
-            ``maximum_period``, ``frequency``, ``period``, ``nterms``,
-            ``nyquist_factor``, ``oversample_factor``, ``freq_unit``,
-            ``normalization``, ``ls_method``.
-        
+    gaia : Boolean
+        True if internet access is available and user wants to display gaia information; User should enter False otherwise.
+    magnitude_limit: float
+        Lower limit of gaia magnitudes to search for.
+    frequencies: list
+        List of frequencies desired to localize the source location for.
+    frequnit: astropy.units.unit
+        Units of the frequencies in frequencies list.
+    principal_components: int
+        Number of components used in PCA for TPF lightcurve.
+    aperture: 2D Boolean array
+        If not specified user the TPF.pipeline_mask will be used, if a user specified aperture is used it must be the same shape as the TPF.
+
+    Returns
+    ----------
+    self.tpf
+        Targetpixelfile object.
+    self.aperture
+        Aperture used.
+    self.raw_lc
+        Lightcurve before PCA.
+    self.dm
+        Design matrix for PCA
+    self.corrected_lc
+        Lightcurve after PCA
+    self.frequency_list
+        List of frequencies used.
+    self.principal_components
+        Number of principal components removed.
+    self.initial_phases
+        Initial phases fit for the frequencies.
+    self.final_phases
+        Final phases fit for the frequencies.
+    self.heatmap = heats.T
+        3D array of amplitude for each frequency at every pixel in the TPF.
+    self.heatmap_error
+        3D array of amplitude erros for each frequency at every pixel in the TPF.
+    self.timeserieslength
+    self.gaiadata
+    self.location
+        Best fit source location.
+    self.heatmap
+        2D array of composite heatmap for all frequencies.
+    self.starfit
+        Gaia sources and their distances from the fitted location of the source.
+    self.result
+        Result paramters of the fit. Use report_fit(self.report) to view.
+    
+    
     """
     
-    def __init__(self, targetpixelfile, gaia=True, magnitude_limit=18, frequencies=[], frequnit=u.uHz, principle_components = 5,aperture=None, **kwargs):
-
-        #Defining an aperture that will be used in plotting and making empty 2-d arrays of the correct size for masks
-        if targetpixelfile.pipeline_mask.any() == False:
-            self.aperture = aperture
-        else:
-            self.aperture = targetpixelfile.pipeline_mask
+    def __init__(self, targetpixelfile, gaia=True, magnitude_limit=18, 
+                 frequencies=[], frequnit=u.uHz, principal_components = 5, 
+                 aperture=None):
+        
         self.tpf = targetpixelfile
+        #Defining an aperture that will be used in plotting and making empty 2-d arrays of the correct size for masks
+        self.aperture = aperture
+        if self.aperture is None:
+            self.aperture = targetpixelfile.pipeline_mask
+            #if targetpixelfile.pipeline_mask.any() == False:
+                #will add a flag here if no aperture
+        
     
         
         # Make a design matrix and pass it to a linear regression corrector
         self.raw_lc = self.tpf.to_lightcurve(aperture_mask=self.aperture)
-        self.dm = lk.DesignMatrix(self.tpf.flux[:, ~self.tpf.create_threshold_mask()], name='regressors').pca(principle_components)
+        self.dm = lk.DesignMatrix(self.tpf.flux[:, ~self.tpf.create_threshold_mask()], name='regressors').pca(principal_components)
         rc = lk.RegressionCorrector(self.raw_lc)
         corrected_lc = rc.correct(self.dm.append_constant())
         corrected_lc[np.where(corrected_lc.quality == 0)]
         self.corrected_lc = corrected_lc.remove_outliers()
         self.frequency_list = np.asarray((frequencies*frequnit).to(1/u.d))
-        self.principle_components = principle_components
+        self.principal_components = principal_components
         
         def Obtain_Initial_Phase(tpf,corrected_lc,frequency_list):
 
             flux = corrected_lc.flux.value
-            times = corrected_lc.time.value - np.mean(corrected_lc.time.value)
+            times = corrected_lc.time.value - np.nanmean(corrected_lc.time.value)
+            #appending a extra value here to ensure lightkurve doesn't call an error if length of frequencies =1
             pg = corrected_lc.to_periodogram(frequency = np.append([0.0001],frequency_list),ls_method='slow')
             initial_flux= np.asarray(pg.power[1:])
 
@@ -84,7 +124,7 @@ class PixelMapFit:
 
                     model.set_param_hint('f{0:d}phase'.format(i), min = -np.pi, max = np.pi ,value= initial_phase[i],vary = False)
                     model.set_param_hint('f{0:d}amp'.format(i), value = initial_flux[i],vary=False)
-                    model.set_param_hint('height', value= np.mean(flux),vary=False)
+                    model.set_param_hint('height', value= np.nanmean(flux),vary=False)
                     model.set_param_hint('f{0:d}freq'.format(i),value = frequency_list[i], vary = False)
 
 
@@ -102,7 +142,7 @@ class PixelMapFit:
         def Obtain_Final_Phase(tpf,corrected_lc,frequency_list,initial_phases):
 
             flux = corrected_lc.flux.value
-            times = corrected_lc.time.value - np.mean(corrected_lc.time.value)
+            times = corrected_lc.time.value - np.nanmean(corrected_lc.time.value)
             pg = corrected_lc.to_periodogram(frequency = np.append([0.0001],frequency_list),ls_method='slow')
             initial_flux= np.asarray(pg.power[1:])
 
@@ -124,7 +164,7 @@ class PixelMapFit:
 
                 model.set_param_hint('f{0:d}phase'.format(i), min = -np.pi, max = np.pi ,value= initial_phases[i],vary = True)
                 model.set_param_hint('f{0:d}amp'.format(i), value = initial_flux[i],vary=True)
-                model.set_param_hint('height', value= np.mean(flux),vary=True)
+                model.set_param_hint('height', value= np.nanmean(flux),vary=True)
                 model.set_param_hint('f{0:d}freq'.format(i),value = frequency_list[i], vary = False)
 
 
@@ -142,7 +182,7 @@ class PixelMapFit:
         def Obtain_Final_Fit(tpf,corrected_lc,frequency_list,final_phases):
 
             flux = corrected_lc.flux.value
-            times = corrected_lc.time.value - np.mean(corrected_lc.time.value)
+            times = corrected_lc.time.value - np.nanmean(corrected_lc.time.value)
             pg = corrected_lc.to_periodogram(frequency = np.append([0.0001],frequency_list),ls_method='slow')
             initial_flux= np.asarray(pg.power[1:])
 
@@ -164,7 +204,7 @@ class PixelMapFit:
 
                 model.set_param_hint('f{0:d}phase'.format(i), value= final_phases[i],vary = False)
                 model.set_param_hint('f{0:d}amp'.format(i), value = initial_flux[i],vary=True)
-                model.set_param_hint('height', value= np.mean(flux),vary=True)
+                model.set_param_hint('height', value= np.nanmean(flux),vary=True)
                 model.set_param_hint('f{0:d}freq'.format(i),value = frequency_list[i], vary = False)
 
 
@@ -194,28 +234,17 @@ class PixelMapFit:
                 lightcurve = self.tpf.to_lightcurve(aperture_mask=mask)
                 rcc = lk.RegressionCorrector(lightcurve)
                 lc = rcc.correct(self.dm.append_constant())
-                #lc = lc[np.where(lc.quality == 0)]
-                #lc = lc.remove_outliers()
-
-                
-                
-                
+                              
                 bestfit = Obtain_Final_Fit(self.tpf,lc,self.frequency_list,self.final_phases)
                 heat = np.asarray([bestfit.best_values['f{0:d}amp'.format(n)] for n in np.arange(len(self.frequency_list))])
-                #heat = bestfit.best_values['f0amp']# / bestfit.params['f0amp'].stderr
                 heat_error =  np.asarray([bestfit.params['f{0:d}amp'.format(n)].stderr for n in np.arange(len(self.frequency_list))])
                 
                 #Extending the list of fitting data for each pixel
                 heats.extend([heat])
                 heats_error.extend([heat_error])
-                
-        #Taking the final list and turning it into a 2-d numpy array with the same dimensions of the full postage stamp      
-        #heats = np.reshape(np.asarray(heats),(len(self.aperture),len(self.aperture[0])))
-        #heats_error = np.reshape(np.asarray(heats_error),(len(self.aperture),len(self.aperture[0])))
+                    
         heats = np.asarray(heats)
         heats_error = np.asarray(heats_error)
-        
-        #Defining self.periodogram as this 2-d array of periodogram data
         self.heatmap = heats.T
         self.heatmap_error = heats_error.T
         self.timeserieslength = (self.tpf.time.max()-self.tpf.time.min()).value
@@ -257,7 +286,7 @@ class PixelMapFit:
 
 
 
-            # Gently size the points by their Gaia magnitude
+            # Size the points by their Gaia magnitude
             sizes = 64.0 / 2**(result['Gmag']/5.0)
             one_over_parallax = 1.0 / (result['Plx']/1000.)
             source = dict(ra=result['RA_ICRS'],
@@ -280,38 +309,44 @@ class PixelMapFit:
                 self.heatmap_error = heats_error
                 self.size = tpf.pipeline_mask.shape
                 self.frequencies= frequencies
-            def centroid(self):
+            def location(self):
                 
                 #Residuals to minimize relative to the error bars
                 def residual(params, amp, amperr):
-                    
+
                     x = params['x']
                     y = params['y']
                     sigma = params['sigma']
 
-                    xpix,ypix = np.meshgrid(np.arange(self.size[0]),np.arange(self.size[1]))
-                    
+
+
                     res = []
-                    for i in np.arange(len(self.frequencies)):
+                    for i in np.arange(len(frequencies)):
                         height = params['height{0:d}'.format(i)]
-                        
-                        model = height*np.exp(-(((x-xpix)/sigma)**2+((y-ypix)/sigma)**2)/2)
-                        
+
+                        prf = PRF.Gaussian_PRF(sigma)
+                        model = height*prf.locate(x+.5, y+.5, (self.size[0],self.size[1]))
+
                         res.extend( [(amp[i].reshape(self.size)-model) / amperr[i].reshape(self.size)])
-                        
+
 
                     return np.asarray(res)
 
                 #Set starting values to converge from
                 self.heatmap_error[np.where(self.heatmap_error==None)]=np.nan
+                self.heatmap_error[np.where(self.heatmap_error==0)]=np.nan
+                #reshaping composite heatmap for user to plot
                 composite_heatmap = self.heat_stamp.sum(axis=0).reshape(self.size) / ((np.nansum(self.heatmap_error**2,axis=0))**(1/2)).reshape(self.size)#issue with numpy using sqrt?
                 c = np.where(composite_heatmap==composite_heatmap.max())
+
+
                 params = Parameters()
-                for i in np.arange(len(frequencies)):
+                for i in np.arange(len(frequencies)):#
                     params.add('height{0:d}'.format(i), value=np.max(self.heat_stamp[i]))
-                params.add('x', value=c[1][0]) 
-                params.add('y', value=c[0][0])
+                params.add('x', value=c[1][0])#c[0]) 
+                params.add('y', value=c[0][0])#c[1])
                 params.add('sigma', value=1)
+
                 
                 #Do the fit
                 minner = Minimizer(residual, params, fcn_args=(self.heat_stamp, self.heatmap_error))
@@ -335,38 +370,53 @@ class PixelMapFit:
                                  source = np.asarray(gaia_data['source']),
                                  x = np.asarray(gaia_data['x']),
                                  y = np.asarray(gaia_data['y']),
-                                 distance = distances,
-                                 probability = 2*stats.norm.sf(distances,scale=np.sqrt(self.result.params['x'].stderr**2 +self.result.params['y'].stderr**2 )))#I believe mutiply by 2 since we wont have a negative distance
+                                 distance = distances)
+                                 #include a probability metric here eventually 
+                        
                     starlist = pd.DataFrame.from_dict(stars)
                     self.stars = starlist.sort_values(by=[r'distance'])
                     
         fh = frequency_heatmap(self.tpf,self.heatmap,self.heatmap_error,self.frequency_list,self.gaiadata) 
-        fh.centroid()
+        fh.location()
         fh.star_list()
-        self.centroid = [fh.x,fh.y]
+        self.location = [fh.x,fh.y]
         self.heatmap = self.heatmap.sum(axis=0).reshape(self.aperture.shape[0],self.aperture.shape[1]) / np.sqrt((self.heatmap_error**2).sum(axis=0)).reshape(self.aperture.shape[0],self.aperture.shape[1])
         self.starfit= fh.stars.reset_index()
         self.result = fh.result
     
     def info(self):
         plt.imshow(self.heatmap,origin='lower')
-        #plot the centroid
+        #plot the location
         if (self.gaiadata != None):
             plt.scatter(self.gaiadata['x'],self.gaiadata['y'],s=self.gaiadata['size']*5,c='white',alpha=.6)
-            plt.scatter(self.centroid[0],self.centroid[1],marker='X',s=100)
+            plt.scatter(self.location[0],self.location[1],marker='X',c='black',s=70)
             
         plt.xlim(-.5,self.aperture.shape[1]-1+.5)
         plt.ylim(-.5,self.aperture.shape[0]-1+.5)
         print(self.starfit)
         report_fit(self.result)
+        if (np.asarray([self.result.params['height{0:d}'.format(j)].stderr for j in np.arange(len(self.frequency_list))]) / np.asarray([self.result.params['height{0:d}'.format(j)].value for j in np.arange(len(self.frequency_list))])).any() >.2:
+            print(Warning('Frequencies used may not all belong to the same source and provided fit could be unreliable'))
     
     def pca(self):
         plt.figure(figsize=(12,5))
-        plt.plot(self.tpf.time.value, self.dm.values + np.arange(self.principle_components)*0.2)
-        plt.title('Principle Components Contributions')
+        plt.plot(self.tpf.time.value, self.dm.values + np.arange(self.principal_components)*0.2)
+        plt.title('Principal Components Contributions')
         plt.xlabel('Offset')
         g2 = self.raw_lc.plot(label='Raw light curve')
         self.corrected_lc.plot(ax=g2, label='Corrected light curve')
 
         plt.show()
 
+    def plot(self,save = None,figuresize = (10,10)):
+        plt.figure(figsize = (figuresize))
+        plt.imshow(self.heatmap,origin='lower')
+        
+        if (self.gaiadata != None):
+            plt.scatter(self.gaiadata['x'],self.gaiadata['y'],s=self.gaiadata['size']*5,c='white',alpha=.6)
+            plt.scatter(self.location[0],self.location[1],marker='X',c ='black',s=70)
+            
+        plt.xlim(-.5,self.aperture.shape[1]-1+.5)
+        plt.ylim(-.5,self.aperture.shape[0]-1+.5)
+        if save != None:
+            plt.savefig(save)
